423c65cb59454424bbf0d3aaade34e05523d4da7
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-09-03 09:27:12 -0300

    Header criado, falta separar funções.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,54 @@
 'use client'
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const token = localStorage.getItem('token');
-  if(token){
-    return <div>oi</div>
-  }
+  const email = localStorage.getItem('email')
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    const fetching = async () => {
+      try {
+        const response = await fetch(`http://localhost:3333/me/${email}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+    
+        if (response.ok) {
+          const user = await response.json();
+          setUserName(user.name)
+          console.log(userName);
+        } else {
+          console.error('Erro ao buscar dados do usuário.');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a solicitação:', error);
+      }
+    };
+    
+    fetching();
+  }, [])
+  
+  
+  
+
   return (
     <div className="w-full bg-zinc-900">
       <div className="flex justify-around text-white p-5">
         <div>
-        <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
         </div>
         <div>
-          
-          <Link href="/login">Login</Link>
+          {token ? (
+            <p>{userName}</p>
+          ) : (
+            <Link href="/login">Login</Link>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
