e37cfd98e47b4b78398af191991af50c88ac61a7
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-09-05 14:50:23 -0300

    token in da useeffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-'use client'
-import React from 'react'
+  'use client'
+  import React, { useEffect } from 'react'
 
-export default function Home() {
-  const token = localStorage.getItem('token')
-  if(!token){
-    window.location.href = '/login';
+  export default function Home() {
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        window.location.href = '/login';
+      }
+    }, []);
+    return (
+      <button onClick={()=>{
+        localStorage.removeItem('token')
+        localStorage.removeItem('email')
+        window.location.href = '/login';
+      }}>Logout</button>
+    )
   }
-  return (
-    <button onClick={()=>{
-      localStorage.removeItem('token')
-      localStorage.removeItem('email')
-      window.location.href = '/login';
-    }}>Logout</button>
-  )
-}
