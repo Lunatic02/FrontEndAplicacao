2d43fc961df50a04af618db475e17609a953be04
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-09-06 15:16:50 -0300

    Tipagem Dates

--- src/utils/filterTransactionsByPeriod.ts
+++ src/utils/filterTransactionsByPeriod.ts
@@ -7,7 +7,7 @@ export const last7Days = subDays(today, 7);
 export const last30Days = subMonths(today, 1);
 export const lastYear = subYears(today, 1);
 
-export const filterTransactionsByPeriod = (startDate, endDate, transactions :  Transaction[]) => {
+export const filterTransactionsByPeriod = (startDate: Date, endDate: Date, transactions :  Transaction[]) => {
   return transactions.filter((transaction) =>
     isWithinInterval(new Date(transaction.expentAt), { start: startDate, end: endDate })
   );
